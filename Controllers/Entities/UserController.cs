417a345b8ab2124af7caf50acbd0dc3030c3476e
Shane Jemmotte <Shane.Jemmotte@isams.co.uk>
2020-01-30 15:46:43 +0000

    updated codebase to adhere to better routing

---
+++ Controllers/Entities/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyHome.Model.Entities;
+
+namespace MyHome.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return new List<User>();
+        }
+    }
+}
